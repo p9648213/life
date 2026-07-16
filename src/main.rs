70d1f4bf6113a909fd2ddcc300cd069941b1a2bd
so <p9648213750912@gmail.com>
2026-07-16 07:37:53 +0700

    716

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use life::{
         response::{Response, StatusCode},
     },
     server::Server,
+    templates
 };
 
 fn home<'buf, 'req>(_: &'req Request<'buf>) -> Response<'req> {
+    let mut html = String::new();
+    templates::render_form(&mut html);
+    Response::html(StatusCode::Ok, &html)
+}
+
+fn form_post<'buf, 'req>(_: &'req Request<'buf>) -> Response<'req> {
     let html = String::new();
     Response::html(StatusCode::Ok, &html)
 }
@@ -14,5 +21,6 @@ fn home<'buf, 'req>(_: &'req Request<'buf>) -> Response<'req> {
 fn main() -> std::io::Result<()> {
     let mut server = Server::new();
     server.routes.get("/", home);
+    server.routes.post("/demo/form", form_post);
     server.run("127.0.0.1:8080")
 }
