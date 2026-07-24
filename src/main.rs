334b70dd8704e1e62b73209bccf65884647105e3
so <p9648213750912@gmail.com>
2026-07-24 08:16:35 +0700

    724

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,29 @@ use life::{
         response::{Response, StatusCode},
     },
     server::Server,
-    templates,
+    templates::render_resource,
 };
 
-fn home<'buf, 'req>(_: &'req Request<'buf>) -> Response<'req> {
-    let mut html = String::new();
-    templates::render_form(&mut html);
-    Response::html(StatusCode::Ok, &html)
+fn create_resourse<'buf, 'req>(request: &'req Request<'buf>) -> Response<'req> {
+    if let Ok(form) = request.extract_form(["r_name"]) {
+        println!("{:?}", form);
+        Response::html(StatusCode::Ok, "Resource created")
+    } else {
+        Response::text_plain(StatusCode::InternalServerError, "Error Parsing Form")
+    }
 }
 
-fn form_post<'buf, 'req>(req: &'req Request<'buf>) -> Response<'req> {
-    let result = req.extract_form(["name", "message"]);
-    match result {
-        Ok(values) => {
-            let name = &values[0];
-            let message = &values[1];
-            let html = format!("<div>Name: {} - Message: {}</div>", name, message);
-            Response::html(StatusCode::Ok, &html)
-        }
-        Err(error) => Response::text_plain(StatusCode::BadRequest, &error.to_string()),
-    }
+fn list_resourse<'buf, 'req>(request: &'req Request<'buf>) -> Response<'req> {
+    let query = request.query().get("id");
+    println!("{:?}", query);
+    let mut html = String::new();
+    render_resource(&mut html);
+    Response::html(StatusCode::Ok, &html)
 }
 
 fn main() -> std::io::Result<()> {
     let mut server = Server::new();
-    server.routes.get("/demo/form", home);
-    server.routes.post("/demo/form", form_post);
+    server.routes.post("/resources", create_resourse);
+    server.routes.get("/resources", list_resourse);
     server.run("127.0.0.1:8080")
 }
